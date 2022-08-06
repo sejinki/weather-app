4d8a24c9419d11ccb77c835d733f8bbc288bb7a6
Sejin <kisejin2015@gmail.com>
2022-08-06 10:23:28 -0600

    five day

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function cityWeather(city) {
         console.log(data);
         var lat = data[0].lat
         var lon = data[0].lon
-        var onecall =`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=imperial`
+        var onecall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=imperial`
         var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=0a9af93029210912aafc391ead4bc3e3`
         fetch(onecall).then(function (response) {
             return response.json()
@@ -32,13 +32,29 @@ function cityWeather(city) {
                 <div class="alert alert-info" role="alert">
                 <h2 class="city">${city}</h2>
             <h2 class="temp">${data.current.temp}</h2>
-            <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png" alt="" class="icon" />
-            <div class="description">${data.current.weather[0].description}</div>
-            <div class="humidity">${data.current.humidity}</div>
-            <div class="wind">${data.current.wind_speed}</div>
-            <div class="UV-Index">${data.current.uvi}</div>
+            <img src="https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png" alt="" class="icon" />
+            <div class="description">Description: ${data.current.weather[0].description}</div>
+            <div class="humidity">Humidity: ${data.current.humidity}</div>
+            <div class="wind">Wind Speed: ${data.current.wind_speed}</div>
+            <div class="UV-Index">UVI-Index: ${data.current.uvi}</div>
 </div>
                 `
+                var fiveHTML = ""
+                for (let i = 1; i < 6; i++) {
+                                    fiveHTML += `
+                                    <div class="card" style="width: 18rem;">
+                <img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${moment.unix(data.daily[i].dt).format("dddd, MMMM Do YYYY")}</h5>
+                    <p class="card-text">Temp:${data.daily[i].temp.day} </p>
+                    <p class="card-text">UVI:${data.daily[i].uvi} </p>
+                    <p class="card-text">Humidity:${data.daily[i].humidity} </p>
+                    <p class="card-text">description:${data.daily[i].weather[0].description} </p>
+                    <p class="card-text">wind speed:${data.daily[i].wind_speed} </p>
+                </div>
+                </div>`
+                }
+                document.querySelector(".fiveDay").innerHTML = fiveHTML
                 // document.querySelector(".city").textContent = "The weather in " + city
                 // document.querySelector(".temp").textContent = Math.round(data.main.temp) + String.fromCharCode(176);
                 // document.querySelector(".description").textContent = data.weather[0].main;
